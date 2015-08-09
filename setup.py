ecd3839c40303870ac9dbcfc9d9507264ba7162b
M. de Verteuil <onceuponajooks@gmail.com>
2015-08-09 19:43:02 -0400

    Get pytest up to snuff

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
 import setuptools
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setuptools.setup(
     name="jenkins-github-webhook-plus",
@@ -14,6 +36,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
 
     install_requires=['Flask>=0.10.1', 'requests>=2.0.0'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
